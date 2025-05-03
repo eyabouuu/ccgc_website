2647494c2389abbc81b0fb231334ba4a8e9e4754
eyabouuu <eyabouabdallah27@gmail.com>
2025-05-03 18:53:53 +0000

    webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,7 +10,24 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                  require('postcss-calc')({
+                    exclude: /@radix-ui\/themes/, // Exclude Radix UI styles
+                  }),
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
