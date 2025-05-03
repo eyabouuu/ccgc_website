2a7899bf8c99da9c7407f47efe07c54bd0ac1a05
eyabouuu <eyabouabdallah27@gmail.com>
2025-05-03 15:50:47 +0000

    auth middle ware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 const isAuthenticated = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: 'No token' });
